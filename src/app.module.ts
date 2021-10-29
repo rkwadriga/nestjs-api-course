19127903a1c7bcb020ef9a278bdbcb20cb552814
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-10-29 23:12:59 +0300

    Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppDummy } from './app.dummy';
 import { AppJapanService } from './app.japan.service';
 import { AppService } from './app.service';
-import { Event } from './events/event.entity';
+import ormConfig from './config/orm.config';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-      type: 'mysql',
-      //host: '172.17.0.1',
-      host: 'localhost',
-      port: 3304,
-      username: 'admin',
-      password: 'admin',
-      database: 'nest_events',
-      entities: [Event],
-      synchronize: true
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true // Allows to do something like "SUPPORT_EMAIL=support@${APP_URL}" in .env files
     }),
+    TypeOrmModule.forRootAsync({useFactory: ormConfig}),
     EventsModule
   ],
   controllers: [AppController],
---
+++ src/config/orm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Event } from "src/events/event.entity";
+
+export default (): TypeOrmModuleOptions => ({
+    type: 'mysql',
+    //host: '172.17.0.1',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Event],
+    synchronize: true
+});
