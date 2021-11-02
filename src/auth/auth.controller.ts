1652406463c6d5580905e845a7daa9775927a7f1
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-11-02 22:52:44 +0200

    Generate a JWT token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,20 @@
 import {Controller, Post, Request, UseGuards} from "@nestjs/common";
 import {AuthGuard} from "@nestjs/passport";
+import {AuthService} from "./auth.service";
 
 
 @Controller('/auth')
 export class AuthController {
-
+    constructor(
+        private readonly authService: AuthService
+    ) {}
+    
     @Post('/login')
     @UseGuards(AuthGuard('local')) // "local" is default name of guard (the second argument in PassportStrategy's controller - look at the src/auth/local.strategy.ts)
     async login(@Request() request) {
         return {
             userID: request.user.id,
-            token: 'The token will go here'
+            token: this.authService.getTokenForUser(request.user)
         };
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,23 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./user.entity";
 import {LocalStrategy} from "./local.strategy";
 import {AuthController} from "./auth.controller";
+import {JwtModule} from "@nestjs/jwt";
+import {AuthService} from "./auth.service";
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [LocalStrategy],
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        JwtModule.registerAsync({
+            useFactory: () => ({
+                secret: process.env.AUTH_SECRET,
+                    signOptions: {
+                    expiresIn: '63m'
+                }
+            })
+        })
+    ],
+    providers: [LocalStrategy, AuthService],
     controllers: [AuthController]
 })
 export class AuthModule {
