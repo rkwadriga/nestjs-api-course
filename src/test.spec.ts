705ac51f9510db4d82ca6c318f3dadce5d6dd192
rkwadriga <dmitry.kushneriov@obreey-products.com>
2021-11-10 15:15:19 +0200

    Install "NEXT" framework for testing and create a simple test

---
+++ src/test.spec.ts
@@ -0,0 +1,13 @@
+
+
+test('Test is null', () => {
+    const n = null;
+    
+    expect(n).toBeNull();
+});
+
+test('2+2', () => {
+    const result = 2 + 2;
+    
+    expect(result).toBe(4);
+});
